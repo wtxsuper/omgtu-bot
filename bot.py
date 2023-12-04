a161c3e9087c5e126cc02dd50411c8056de69fb5
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2023-12-04 18:39:00 +0600

    Add calendar and buttons

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
-import asyncio
-import logging
-import threading
+from datetime import datetime
 
-import aiogram.utils.token
-from aiogram import Bot, Dispatcher, Router, types
+from aiogram import Bot, Dispatcher, F
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
-from aiogram.utils.markdown import *
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import Message, CallbackQuery, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.utils.markdown import *
+from aiogram_calendar import SimpleCalendar, get_user_locale, SimpleCalendarCallback
 
 import main
 from messages import error
@@ -20,6 +19,9 @@ dp = Dispatcher()
 class User(StatesGroup):
     group = State()
     date = State()
+
+
+weekdays = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"]
 
 
 async def bot_start(BOT_TOKEN: str) -> None:
@@ -52,21 +54,58 @@ async def group_select(message: Message, state: FSMContext) -> None:
     await state.update_data(group=message.text)
     await state.set_state(User.date)
     await message.answer("Группа успешно выбрана!")
+    await message.answer("Выбери дату, на которую хочешь отобразить расписание:", reply_markup=await SimpleCalendar(
+        locale=await get_user_locale(message.from_user)).start_calendar(year=datetime.now().year,
+                                                                        month=datetime.now().month))
 
 
-@dp.message(Command("change"))
-async def group_change(message: Message, state: FSMContext) -> None:
+# simple calendar usage - filtering callbacks of calendar format
+@dp.callback_query(SimpleCalendarCallback.filter())
+async def process_calendar(callback_query: CallbackQuery, callback_data: SimpleCalendarCallback) -> None:
+    selected, date = await SimpleCalendar(locale=await get_user_locale(callback_query.from_user)).process_selection(
+        callback_query, callback_data)
+    if selected:
+        builder = InlineKeyboardBuilder()
+        builder.add(InlineKeyboardButton(text="Другая дата 📅", callback_data="date"))
+        builder.add(InlineKeyboardButton(text="Сменить группу 🔁", callback_data="group"))
+        builder.add(InlineKeyboardButton(text="Найти преподавателя 🔎", callback_data="find"))
+        builder.adjust(1)
+        # TODO Вывод расписания
+        await callback_query.message.answer(
+            f'''Расписание на {hbold(weekdays[date.weekday()].lower())}, {hbold(date.strftime("%d.%m.%Y"))}\nВ разработке...''',
+            reply_markup=builder.as_markup())
+        await callback_query.answer()
+
+
+@dp.callback_query(F.data == "group")
+async def group_change(callback: CallbackQuery, state: FSMContext) -> None:
     await state.set_state(User.group)
-    await message.answer("Введи новый номер группы:")
+    await callback.message.answer("Введи новый номер группы:")
+    await callback.answer()
+
+
+@dp.callback_query(F.data == "date")
+async def group_change(callback: CallbackQuery) -> None:
+    await callback.message.answer("Выбери дату, на которую хочешь отобразить расписание:",
+                                  reply_markup=await SimpleCalendar(
+                                      locale=await get_user_locale(callback.from_user)).start_calendar(
+                                      year=datetime.now().year, month=datetime.now().month))
+    await callback.answer()
+
+
+@dp.callback_query(F.data == "find")
+async def group_change(callback: CallbackQuery) -> None:
+    builder = InlineKeyboardBuilder()
+    builder.button(text="Вернуться к расписанию 🔙", callback_data="date")
+    await callback.message.answer("Пока ещё не реализовано...", reply_markup=builder.as_markup())
+    await callback.answer()
 
 
 @dp.message(Command("debug"))
 async def debug(message: Message, state: FSMContext) -> None:
     if main.DEBUG_MODE == "1":
-        await message.answer(
-            f"{hbold('User ID')}: {message.from_user.id}\n"
-            f"{hbold('State')}: {await state.get_state()}\n"
-            f"{hbold('DState')}: {await state.get_data()}"
-        )
+        await message.answer(f"{hbold('User ID')}: {message.from_user.id}\n"
+                             f"{hbold('State')}: {await state.get_state()}\n"
+                             f"{hbold('DState')}: {await state.get_data()}")
     else:
         return
